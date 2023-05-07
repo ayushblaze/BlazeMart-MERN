f7ce6f40be1bed307e013ec42f51b43b0eff1e2a
Ayush Singh <ayushsops@gmail.com>
2023-05-07 07:27:09 +0530

    Dummy user data

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from "bcryptjs";
+
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@blazemart.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "Ayush",
+    email: "ayush@blazemart.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Sonal",
+    email: "admin@blazemart.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+];
+
+export default users;
